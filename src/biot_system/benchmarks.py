6c87fc232fa7a2cc82aeb7e608b03d754a878734
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2021-07-02 14:23:53 +0200

    Added manufactured solutions

---
+++ src/biot_system/benchmarks.py
@@ -0,0 +1,44 @@
+import fenics as pde
+
+zero = pde.Constant(0)
+
+
+def static_U(x, t, scale):
+    U = pde.as_vector((
+        scale*pde.cos(x[0])*pde.cos(x[1]),
+        scale*pde.cos(x[0])*pde.cos(x[1])
+    ))
+    U_dot = pde.as_vector((
+        zero*x[0],
+        zero*x[0]
+    ))
+
+    return U, U_dot
+
+
+def static_P(x, t, scale):
+    P = scale*pde.cos(x[0])*pde.cos(x[1])
+    P_dot = zero*x[0]
+    return P, P_dot
+
+
+def linear_time_U(x, t, scale):
+    U = pde.as_vector((
+        scale*pde.cos(x[0])*pde.cos(x[1])*t,
+        scale*pde.cos(x[0])*pde.cos(x[1])*t
+    ))
+    U_dot = pde.as_vector((
+        scale*pde.cos(x[0])*pde.cos(x[1]),
+        scale*pde.cos(x[0])*pde.cos(x[1])
+    ))
+
+    return U, U_dot
+
+
+def linear_time_P(x, t, scale):
+    scale = pde.Constant(scale)
+    P = scale*pde.cos(x[0])*pde.cos(x[1])*t
+    P_dot = scale*pde.cos(x[0])*pde.cos(x[1])
+    return P, P_dot
+
+
